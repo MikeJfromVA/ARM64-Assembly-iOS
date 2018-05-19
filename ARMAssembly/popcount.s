4d0b4142236b1d72b494b0da447bad012e6d704e
Michael Johnson <mike@mikejfromva.com>
2018-05-19 16:14:20 -0400

    Assembly compiles and linker finds the function. No crashes.

--- ARMAssembly/popcount.s
+++ ARMAssembly/popcount.s
@@ -6,3 +6,25 @@
 //  Copyright © 2018 Michael Johnson. All rights reserved.
 //
 
+#ifdef __arm64__
+
+    .p2align 2
+    .global _popcount
+
+_popcount:
+    .cfi_startproc
+    sub     sp, sp, #16
+    .cfi_def_cfa_offset 16
+    adds            x0, x0, #15
+    cset     w8, vs
+    str     x0, [sp, #8]
+    str     w8, [sp, #4]
+    b.vs    LBB1_2
+    ldr     x0, [sp, #8]
+    add     sp, sp, #16
+    ret
+LBB1_2:
+    brk     #0x1
+    .cfi_endproc
+
+#endif
