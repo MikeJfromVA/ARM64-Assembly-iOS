d04fd9c5d3115bc5bd021893012978bb30b87d71
Michael Johnson <mike@mikejfromva.com>
2018-05-20 09:18:30 -0400

    Popcount in Aarch64 assembly working for values up to 255

--- ARMAssembly/popcount.s
+++ ARMAssembly/popcount.s
@@ -8,20 +8,22 @@
 
 #ifdef __arm64__
 
-    .p2align 2
     .global _popcount
+    .align 4
 
 _popcount:
     .cfi_startproc
     sub     sp, sp, #16
     .cfi_def_cfa_offset 16
-    adds            x0, x0, #15
-    cset     w8, vs
-    str     x0, [sp, #8]
-    str     w8, [sp, #4]
-    b.vs    LBB1_2
-    ldr     x0, [sp, #8]
-    add     sp, sp, #16
+    dup         v0.2d, x0
+    cnt         v0.16b, v0.16b
+    fmov        x0, v0.d[1]
+    cset        w8, vs
+    str         x0, [sp, #8]
+    str         w8, [sp, #4]
+    b.vs        LBB1_2
+    ldr         x0, [sp, #8]
+    add         sp, sp, #16
     ret
 LBB1_2:
     brk     #0x1
